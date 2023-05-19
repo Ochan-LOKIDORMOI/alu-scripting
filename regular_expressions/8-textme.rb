cc302794907a1a0a108573759d81184c81e19320
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-05-19 01:22:24 -0700

    Textme

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #This script must contain sender,receiver and flag
-puts ARGV[0].scan(/\[Sender:(.*?\)]\s\[Receiver:(.*?\)]\s\[Flags:(.*?\)\]/).join(',')
+puts ARGV[0].scan(/\[Sender:(.*?)\]\s\[Receiver:(.*?)\]\s\[Flags:(.*?)\]/).join(',')
