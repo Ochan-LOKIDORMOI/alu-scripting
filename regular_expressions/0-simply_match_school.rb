3986f66c71aa44769a32184dd918ef9e1dc153b2
Ochan-LOKIDORMOI <o.lokidormo@alustudent.com>
2023-05-19 00:20:17 -0700

    Simply matching School

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#A script that accepts one argument and and pass it to regular expression
-puts ARGV[0].scan(/school/).join
+#A script that accepts one argument and pass to regular expression
+puts ARGV[0].scan(/School/).join
